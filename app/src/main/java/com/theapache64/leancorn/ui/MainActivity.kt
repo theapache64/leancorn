57fd1ca57d822fcbd290f4def7dc4d431a8bb378
theapache64 <theapache64@gmail.com>
2021-10-10 00:00:03 +0530

    🌛 first commit

---
+++ app/src/main/java/com/theapache64/leancorn/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.theapache64.leancorn.ui
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.theapache64.leancorn.R
+
+/**
+ * Loads [MainFragment].
+ */
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
